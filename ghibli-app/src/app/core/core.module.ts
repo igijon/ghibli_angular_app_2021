b5b214dedd8ed4bd052401373087335dce49d758
Inma Gijón <igijoninf@gmail.com>
2021-11-23 10:48:30 +0100

    ghibli app core module, navbar, footer

---
+++ ghibli-app/src/app/core/core.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoreRoutingModule } from './core-routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    CoreRoutingModule
+  ],
+  exports: [
+    NavbarComponent,
+    FooterComponent
+  ]
+})
+export class CoreModule {
+}
